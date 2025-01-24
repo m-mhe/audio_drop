5a1686821a72ebdc3a7acba169e0172dc11061f5
Momin Hosan Emon <mominhasanemon@gmail.com>
2025-01-24 16:53:32 +0600

    Home screen first widget

--- audio_drop/lib/app_ui/home_screen.dart
+++ audio_drop/lib/app_ui/home_screen.dart
@@ -1,3 +1,5 @@
+import 'package:audio_drop/app_utils/image_paths.dart';
+import 'package:audio_drop/app_utils/light_theme_color.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -6,8 +8,62 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: Center(
-        child: Text("Home Screen"),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: (MediaQuery.sizeOf(context).width / 3.5),
+              child: ListView.builder(
+                  itemCount: 69,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (BuildContext context, i) {
+                    return Padding(
+                      padding: const EdgeInsets.only(left: 10),
+                      child: Column(
+                        children: [
+                          Container(
+                            height:
+                                (MediaQuery.sizeOf(context).width / 3.5) / 1.1,
+                            width: (MediaQuery.sizeOf(context).width / 3.5),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(7),
+                                image: DecorationImage(
+                                    image:
+                                        AssetImage(ImagePaths.defaultThumbnail),
+                                    fit: BoxFit.fitWidth)),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                Container(
+                                  height: ((MediaQuery.sizeOf(context).width /
+                                              3.5) /
+                                          1.1) /
+                                      2.5,
+                                  width:
+                                      (MediaQuery.sizeOf(context).width / 3.5),
+                                  decoration: BoxDecoration(
+                                      color: LightThemeColor.blueTwo,
+                                      borderRadius: BorderRadius.only(
+                                          bottomLeft: Radius.circular(7),
+                                          bottomRight: Radius.circular(7),
+                                          topLeft: Radius.circular(14),
+                                          topRight: Radius.circular(14))),
+                                  child: Center(
+                                    child: Text("${(((69 - i) * 69) + 478)}",
+                                      style: TextStyle(color: LightThemeColor.blueFour, fontWeight: FontWeight.w700, fontSize: 15),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  }),
+            )
+          ],
+        ),
       ),
     );
   }
